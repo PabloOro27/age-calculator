f7a0d938c4a85f0b123e223af6740d7c84b63a09
PabloOro27 <pabloorozco2712@gmail.com>
2023-08-11 22:54:19 -0600

    se agrego la parte de resultado

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,19 +5,62 @@ export const GeneralContext = createContext();
 // funcion proveedor del contexto 
 export const GeneralContextProvider = ({ children }) => {
   // funciones y estados
+  const [formData, setFormData] = useState({});
+  const [fechaActual, setFechaActual] = useState("");
+  const [fechaResultado, setFechaResultado] = useState({
+    year: "--",
+    month: "--",
+    day: "--",
+  });
   // Obtener la fecha actual
   const currentDate = new Date();
 
-  // Extraer los componentes de la fecha (año, mes, día, hora, minutos, segundos)
-  const year = currentDate.getFullYear();
-  const month = currentDate.getMonth() + 1; // Los meses en JavaScript son indexados desde 0
-  const day = currentDate.getDate();
-    console.log(`fecha actual: ${year}-${month}-${day}`);
+  useEffect(() => {
+    // Extraer los componentes de la fecha (año, mes, día, hora, minutos, segundos)
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1; // Los meses en JavaScript son indexados desde 0
+    const day = currentDate.getDate();
+    setFechaActual(`${year}-${month}-${day}`);
+  }, []);
+  // FUNCION DE CALCULO DE FECHAS 
+  const useDateDiff = () => {
+    let fechaDiff =
+      new Date(
+        `${formData.year}-${formData.month}-${formData.day}`
+      ) - new Date(fechaActual);
+    // resultado de año
+    let fechaDiffYear = Math.floor(fechaDiff / (1000 * 60 * 60 * 24 * 365));
+    // resultado de mes
+    let fechaDiffMonth;
+    if (fechaDiffYear > 0) {
+      fechaDiff -= fechaDiffYear * (1000 * 60 * 60 * 24 * 365);
+      fechaDiffMonth = Math.floor(fechaDiff / (1000 * 60 * 60 * 24 * 30));
+    } else {
+      fechaDiffMonth = Math.floor(fechaDiff / (1000 * 60 * 60 * 24 * 30));
+    }
+    // resultado de dia
+    fechaDiff -= fechaDiffMonth * (1000 * 60 * 60 * 24 * 30);
+    let fechaDiffDay = Math.floor(fechaDiff / (1000 * 60 * 60 * 24));
+    
+    let fechaResult = {
+      year: fechaDiffYear,
+      month: fechaDiffMonth,
+      day: fechaDiffDay,
+    };
+    setFechaResultado(fechaResult);
+  };
   //---------------------------------------- ------------------ -----------------------------------------------
   return (
     <GeneralContext.Provider
       // valores a compartir
-      value={{}}
+      value={{
+        formData,
+        setFormData,
+        fechaActual,
+        fechaResultado,
+        setFechaResultado,
+        useDateDiff,
+      }}
     >
       {children}
     </GeneralContext.Provider>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,25 +1,45 @@
-import React from 'react';
+import {React, useContext, useState} from 'react';
+// context
+import { GeneralContext } from '../../Context';
 import { RxDoubleArrowDown } from 'react-icons/rx';
 
 const Home = () => {
-  // estado del fomulario
-  const [formData, setFormData] = React.useState({});
+  // context
+  const {
+    formData,
+    setFormData,
+    fechaActual,
+    fechaResultado,
+    setFechaResultado,
+    useDateDiff,
+  } = useContext(GeneralContext);
+  // --------------------------------------------------
+  // función para enviar el formulario
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData);
+    useDateDiff();
   };
+  // dia limite para el mes
+  const [dayLimit, setDayLimit] = useState(31);
   // función para actualizar el estado del formulario
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
+    // dia limite para el mes
+    if (formData.month === 2) {
+      setDayLimit(28);
+    }
+    if (
+      formData.month === 4 ||
+      formData.month === 6 ||
+      formData.month === 9 ||
+      formData.month === 11
+    ) {
+      setDayLimit(30);
+    }
   };
-  // estilo input
-  const inputStyle = {
-    margin: "0",
-    /* Aplicar estilos específicos para ocultar flechas en navegadores WebKit */
-    WebkitAppearance: "none",
-    MozAppearance: "textfield" /* Para Firefox */,
-  }; 
+  // año actual
+  const currentDate = new Date();
 
   // --------------------------------------------------
   return (
@@ -36,12 +56,12 @@ const Home = () => {
               <input
                 type="number"
                 name="day"
-                min={0}
+                min={1}
+                max={dayLimit}
                 value={formData.day || ""}
                 onChange={handleChange}
                 placeholder="DD"
                 className=" border-2 border-gray-300 rounded-lg w-36 h-12 text-3xl font-bold px-4 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:border-transparent"
-                style={inputStyle}
               />
             </div>
             <div className="flex flex-col gap-2">
@@ -49,6 +69,8 @@ const Home = () => {
               <input
                 type="number"
                 name="month"
+                min={1}
+                max={12}
                 value={formData.month || ""}
                 onChange={handleChange}
                 placeholder="MM"
@@ -60,6 +82,8 @@ const Home = () => {
               <input
                 type="number"
                 name="year"
+                min={currentDate.getFullYear() + 1}
+                max={5000}
                 value={formData.year || ""}
                 onChange={handleChange}
                 placeholder="YYYY"
@@ -69,13 +93,32 @@ const Home = () => {
           </form>
         </div>
         {/* calculo */}
-        <div></div>
+        <div>
+          <h2 className="text-7xl font-extrabold mt-8 flex gap-5">
+            <span className="text-purple-700 font-extrabold">
+              {fechaResultado.year}
+            </span>
+            años
+          </h2>
+          <h2 className="text-7xl font-extrabold mt-5 flex gap-5">
+            <span className="text-purple-700 font-extrabold">
+              {fechaResultado.month}
+            </span>
+            meses
+          </h2>
+          <h2 className="text-7xl font-extrabold mt-5 flex gap-5">
+            <span className="text-purple-700 font-extrabold">
+              {fechaResultado.day}
+            </span>
+            dias
+          </h2>
+        </div>
       </div>
-      <button 
+      <button
         onClick={handleSubmit}
-        className='rounded-full w-24 h-24 bg-purple-500 mt-16 text-white flex items-center justify-center hover:bg-purple-700'
+        className="rounded-full w-24 h-24 bg-purple-500 mt-16 text-white flex items-center justify-center hover:bg-purple-700"
       >
-          <RxDoubleArrowDown size={54} />
+        <RxDoubleArrowDown size={54} />
       </button>
     </div>
   );
